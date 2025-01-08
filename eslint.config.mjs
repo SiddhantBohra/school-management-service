36499deb849496ec429fb60140bf7995602d19e7
SiddhantBohra <abhisidds@gmail.com>
2025-01-08 20:42:53 +0530

    chore: initial setup, add eslint, jest

---
+++ eslint.config.mjs
@@ -0,0 +1,30 @@
+import globals from "globals"
+import pluginJs from "@eslint/js"
+import pluginJest from "eslint-plugin-jest"
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+    {
+        files: ["**/*.js"],
+        languageOptions: {
+            sourceType: "commonjs",
+            ecmaVersion: 2022,
+            globals: {
+                ...globals.node,
+                ...globals.jest,
+            },
+        },
+        plugins: {
+            jest: pluginJest,
+        },
+        rules: {
+            ...pluginJs.configs.recommended.rules,
+            "no-unused-vars": "off",
+            "jest/expect-expect": "error",
+            "jest/no-disabled-tests": "warn",
+            "jest/no-focused-tests": "error",
+            "jest/no-identical-title": "error",
+            "jest/valid-expect": "error"
+        },
+    }
+]
